7aff6a2db91026936397e86bcdfaf6c43b4466b9
Krzysztof Myrcik <myrda123@gmail.com>
2018-12-10 04:34:12 +0100

    maskify

--- src/main/java/Maskify.java
+++ src/main/java/Maskify.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Maskify {
+    public static String maskify(String text) {
+        StringBuilder string1 = new StringBuilder();
+        StringBuilder string2 = new StringBuilder();
+        if (text.length() <= 3) {
+            return text;
+        }
+        for (int i = 0; i < text.length() - 4; i++) {
+            string1.append("#");
+        }
+        for (int i = text.length() - 4; i < text.length(); i++) {
+            string2.append(text.charAt(i));
+        }
+        return string1 + string2.toString();
+    }
 }
--- src/test/java/MaskifyTest.java
+++ src/test/java/MaskifyTest.java
@@ -1,5 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MaskifyTest {
 
+    @Test
+    public void maskify() {
+        assertEquals("whatDaFak", "##bcde", Maskify.maskify("aabcde"));
+        assertEquals("aa","abcd", Maskify.maskify("abcd"));
+        assertEquals("aa","", Maskify.maskify(""));
+        assertEquals("aa","1", Maskify.maskify("1"));
+    }
 }
